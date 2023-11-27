dda839ef4906ac399b4c027f1799db65a3adcbc7
LeitnerMi <michael.leitner@gepardec.com>
2023-11-27 15:56:42 +0100

    changed docker build file

--- src/main/docker/build-and-push.sh
+++ src/main/docker/build-and-push.sh
@@ -2,5 +2,5 @@
 
 ./mvnw package
 docker build -f src/main/docker/Dockerfile.jvm -t quarkus/rabbitmq-poc-app-jvm .
-docker tag quarkus/rabbitmq-poc-app-jvm:latest ghcr.io/leitnermi/rabbitmq-poc-app-jvm:latest
+docker tag quarkus/rabbitmq-poc-app-jvm:latest ghcr.io/leitnermi/rabbitmq-poc-app-jvm:topics
 docker push ghcr.io/leitnermi/rabbitmq-poc-app-jvm:latest
