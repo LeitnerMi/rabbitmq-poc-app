95aa923dad9e54e965ec3af2dc40af3bea8ff2fd
LeitnerMi <michael.leitner@gepardec.com>
2024-01-17 09:17:21 +0100

    added create applications files

--- src/main/java/ConsumerBeanBinding.java
+++ src/main/java/ConsumerBeanBinding.java
@@ -8,7 +8,7 @@ import jakarta.inject.Named;
 @Named("consumerBeanBinding")
 public class ConsumerBeanBinding {
     public String doSomething(String body){
-        Log.info(String.format("I received %s", body));
+        Log.info(String.format("I received %s!", body));
         return body;
     }
 }
